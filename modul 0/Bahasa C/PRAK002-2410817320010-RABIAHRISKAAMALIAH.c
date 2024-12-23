1ef1108eb537a641a7828b26bf17423cf6561946
maylia-15 <rabiahriska8@gmail.com>
2024-12-23 11:40:33 +0800

    Add files via upload

---
+++ modul 0/Bahasa C/PRAK002-2410817320010-RABIAHRISKAAMALIAH.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Selamat Pagi, Rabiah Riska Amaliah\n");
+    printf("Selamat Siang, Rabiah Riska Amaliah\n");
+    printf("Selamat Malam, Rabiah Riska Amaliah\n");
+    return 0;
+}
---
+++ modul 0/Bahasa py/PRAK002-2410817320010-RABIAHRISKAMALIAH.py
@@ -0,0 +1,3 @@
+print("Selamat Pagi, Rabiah Riska Amaliah")
+print("Selamat Siang, Rabiah Riska Amaliah")
+print("Selamat Malam, Rabiah Riska Amaliah")
